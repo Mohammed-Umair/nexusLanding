65108e2431b8798eb1016df9fb04771b3f022d0e
prochao <prochaos18@gmail.com>
2024-02-29 11:39:44 +0530

    updated UI and sign up

--- src/app/components/page-components/SignIn.tsx
+++ src/app/components/page-components/SignIn.tsx
@@ -27,96 +27,96 @@ const SignIn = () => {
     const [viewMore, setViewMore] = useState(false)
     const isBig = useIsTab()
 
-    // const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
-    // const [provider, setProvider] = useState<any>(null);
-    // const clientId: any = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENTID
-    // const isMobile = useIsMobile()
+    const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+    const [provider, setProvider] = useState<any>(null);
+    const clientId: any = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENTID
+    const isMobile = useIsMobile()
 
-    // useEffect(() => {
-    //   // setWeb3auth(1)
-    //   // console.log(web3auth);
+    useEffect(() => {
+      // setWeb3auth(1)
+      // console.log(web3auth);
 
-    //   const init = async () => {
-    //     try {
-    //       const web3auth = new Web3Auth({
-    //         clientId,
-    //         web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET, // import {WEB3AUTH_NETWORK} from "@web3auth/base";
-    //         chainConfig: {
-    //           chainNamespace: CHAIN_NAMESPACES.EIP155,
-    //           chainId: "0x1",
-    //           rpcTarget: "https://rpc.ankr.com/eth",
-    //         },
-    //         uiConfig: {
-    //           appName: "Nexus",
-    //           mode: "light", // light, dark or auto
-    //           loginMethodsOrder: ["google", "twitter", "facebook", "apple",],
-    //           logoLight: "https://nexusprotocol.s3.eu-north-1.amazonaws.com/NexusImages/Nexus+logo+mark+Dark.svg",
-    //           logoDark: "https://nexusprotocol.s3.eu-north-1.amazonaws.com/NexusImages/Nexus+logo+mark+Dark.svg",
-    //           defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
-    //           loginGridCol: 3,
-    //           primaryButton: "socialLogin", // "externalLogin" | "socialLogin" | "emailLogin"
-    //         },
-    //       });
-    //       console.log(web3auth);
+      const init = async () => {
+        try {
+          const web3auth = new Web3Auth({
+            clientId,
+            web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET, // import {WEB3AUTH_NETWORK} from "@web3auth/base";
+            chainConfig: {
+              chainNamespace: CHAIN_NAMESPACES.EIP155,
+              chainId: "0x1",
+              rpcTarget: "https://rpc.ankr.com/eth",
+            },
+            uiConfig: {
+              appName: "Nexus",
+              mode: "light", // light, dark or auto
+              loginMethodsOrder: ["google", "twitter", "facebook", "apple",],
+              logoLight: "https://nexusprotocol.s3.eu-north-1.amazonaws.com/NexusImages/Nexus+logo+mark+Dark.svg",
+              logoDark: "https://nexusprotocol.s3.eu-north-1.amazonaws.com/NexusImages/Nexus+logo+mark+Dark.svg",
+              defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+              loginGridCol: 3,
+              primaryButton: "socialLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+            },
+          });
+          console.log(web3auth);
 
-    //       await web3auth.initModal();
-    //       setWeb3auth(web3auth);
+          await web3auth.initModal();
+          setWeb3auth(web3auth);
 
-    //       if (web3auth.status === "connected") {
-    //         setProvider(web3auth.provider);
-    //         // getUserInfo();
-    //         // getAccounts();
-    //       }
-    //       else {
-    //         await web3auth?.connect();
-    //       }
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   };
+          if (web3auth.status === "connected") {
+            setProvider(web3auth.provider);
+            // getUserInfo();
+            // getAccounts();
+          }
+          else {
+            // await web3auth?.connect();
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
-    //   init();
-
-
-    // }, []);
+      init();
 
 
-
-    // const isConnected = async () => {
-    //   if (!web3auth) {
-    //     console.log("web3auth not initialized yet");
-    //     return false;
-    //   }
-    //   return web3auth.status === "connected";
-    // };
-
-    // const login = async () => {
-    //   const _isConnected = await isConnected();
-    //   if (_isConnected) {
-    //     // alert("Already loggedin");
-    //     return;
-    //   }
-    //   try {
-    //     const web3authProvider = await web3auth?.connect();
-    //     setProvider(web3authProvider);
-    //   }
-    //   catch (e) {
-    //     console.log(e)
-    //   }
+    }, []);
 
 
-    // }
 
-    // const logout = async () => {
-    //   const _isConnected = await isConnected();
-    //   if (_isConnected) {
-    //     await web3auth?.logout();
-    //     setProvider(null);
-    //     return;
-    //   }
+    const isConnected = async () => {
+      if (!web3auth) {
+        console.log("web3auth not initialized yet");
+        return false;
+      }
+      return web3auth.status === "connected";
+    };
+
+    const login = async () => {
+      const _isConnected = await isConnected();
+      if (_isConnected) {
+        // alert("Already loggedin");
+        return;
+      }
+      try {
+        const web3authProvider = await web3auth?.connect();
+        setProvider(web3authProvider);
+      }
+      catch (e) {
+        console.log(e)
+      }
 
 
-    // }
+    }
+
+    const logout = async () => {
+      const _isConnected = await isConnected();
+      if (_isConnected) {
+        await web3auth?.logout();
+        setProvider(null);
+        return;
+      }
+
+
+    }
 
 
     return (
@@ -137,7 +137,7 @@ const SignIn = () => {
 
                 <Text color={DEFAULT_COLORS.Light} fontSize='15px' textAlign='center' lineHeight='20px'>Your Nexus experience is one click away</Text>
             </Box>
-            <Button fullWidth background={DEFAULT_COLORS.Blue}><GoogleIcon /> Continue with Google</Button>
+            {/* <Button fullWidth background={DEFAULT_COLORS.Blue}><GoogleIcon /> Continue with Google</Button>
 
             <Flex style={{ marginTop: '1rem', marginBottom: '1rem' }}>
                 <Button fullWidth background={DEFAULT_COLORS.Blue} ><XIcon /></Button>
@@ -154,16 +154,16 @@ const SignIn = () => {
                 ) 
             }
             <Button margin='1rem 0px' fullWidth background={DEFAULT_COLORS.White} color={DEFAULT_COLORS.Blue} border borderColor={DEFAULT_COLORS.Light} onClick={() => setViewMore(!viewMore)}>{viewMore ? 'Show Less' : 'Explore More Login Options'}</Button>
-
+ */}
 
             <Text color={DEFAULT_COLORS.Light} fontSize='13px' textAlign='center'>We do not store any personal information</Text>
-            <Box mt="1rem">
+            {/* <Box mt="1rem">
 
                 <Input background={DEFAULT_COLORS.White} border borderColor={DEFAULT_COLORS.Light} color={DEFAULT_COLORS.Light} fullWidth placeholder='Enter Your Email' />
-            </Box>
+            </Box> */}
             <Box mt={'1rem'}>
 
-                <ButtonWithIcon icon={<IconButton background={DEFAULT_COLORS.White} color={DEFAULT_COLORS.Blue}> <ArrowForwardIcon sx={{ color: DEFAULT_COLORS.Blue }} /></IconButton>} fullWidth background={DEFAULT_COLORS.Blue} >Continue </ButtonWithIcon>
+                <ButtonWithIcon icon={<IconButton background={DEFAULT_COLORS.White} color={DEFAULT_COLORS.Blue}> <ArrowForwardIcon sx={{ color: DEFAULT_COLORS.Blue }} /></IconButton>} fullWidth background={DEFAULT_COLORS.Blue} onClick={() => login()} >Continue </ButtonWithIcon>
             </Box>
         </Box >
 
