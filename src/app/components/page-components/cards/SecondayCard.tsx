4baddd6e4eed21238856eac4555880057362a594
RoshanDasan <edroshan2001@gmail.com>
2024-03-02 10:28:58 +0530

    ready for test2

--- src/app/components/page-components/cards/SecondayCard.tsx
+++ src/app/components/page-components/cards/SecondayCard.tsx
@@ -8,6 +8,7 @@ import Text from '../../utility-components/text/Text'
 import Flex from '../../utility-components/flex/Flex'
 import useIntersectionAnimation from '@/app/hooks/useIntersectionAnimation'
 import { RefObject } from 'react'
+import useIsMobile from '@/app/hooks/useIsMobile'
 
 interface ImageProps {
     image: string
@@ -15,10 +16,11 @@ interface ImageProps {
     descriptionBold: string
 }
 
-const SecondayCard = ({ image, description,descriptionBold }: ImageProps) => {
+const SecondayCard = ({ image, description, descriptionBold }: ImageProps) => {
     const boxRef = useIntersectionAnimation() as RefObject<HTMLDivElement>;
+    const isMobile = useIsMobile()
     return (
-        <Container background={DEFAULT_COLORS.Dark_Light} width='100%' maxWidth='400px' borderRadius='35px' ref={boxRef} hover={true} >
+        <Container background={DEFAULT_COLORS.Dark_Light} width='100%' maxWidth={isMobile ? '350px' : '400px'} borderRadius='35px' ref={boxRef} hover={true} >
             <Grid container>
                 <Grid item lg={3} sm={3.5} xs={3.5}>
                     <button style={{
@@ -36,14 +38,14 @@ const SecondayCard = ({ image, description,descriptionBold }: ImageProps) => {
                         padding: '2rem 1.3rem',
                         border: 'none',
                         // boxShadow: '0px 4px 92.80000305175781px -7px #0879FF';
-                        boxShadow:'0px 4px 92.80000305175781px 1px #0879FF'
+                        boxShadow: '0px 4px 92.80000305175781px 1px #0879FF'
                     }}>
                         <Image src={image} width='40px' />
                     </button>
                 </Grid>
                 <Grid item lg={9} sm={8.5} xs={8.5}>
                     <Flex style={{ width: '100%', height: '100%' }}>
-                        <Text fontSize='20px' lineHeight='25px'><span style={{fontFamily:"SEN bold"}}>{descriptionBold}</span>{description}</Text>
+                        <Text fontSize='20px' lineHeight='25px'><span style={{ fontFamily: "SEN bold" }}>{descriptionBold}</span>{description}</Text>
                     </Flex>
                 </Grid>
             </Grid>
