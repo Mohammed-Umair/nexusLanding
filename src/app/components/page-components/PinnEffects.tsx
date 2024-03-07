9d819a162411adf189953ce1e5a1beca6ffd81f8
RoshanDasan <edroshan2001@gmail.com>
2024-03-07 11:26:57 +0530

    feat: new scroll animation added

--- src/app/components/page-components/PinnEffects.tsx
+++ src/app/components/page-components/PinnEffects.tsx
@@ -9,63 +9,49 @@ import { StaticImageData } from 'next/image'
 import { DEFAULT_COLORS } from '@/app/utils/colors'
 
 interface MyComponentProps {
-    setPinnedImage: (pinnedImage: string) => void;
+    stack: number;
 }
 
-const PinnEffects = ({ setPinnedImage }: MyComponentProps) => {
-    const [hover, setHover] = React.useState<number>(1)
-    const onMouse = (num: number) => {
-        setHover(num)
-        if (num === 1) {
-            setPinnedImage('/Images/pin-main1.png')
-        } else if (num === 2) {
-            setPinnedImage('/Images/pin-main2.png')
-        }
-        else if (num === 3) {
-            setPinnedImage('/Images/pin-main3.png')
-        }
-        else {
-            setPinnedImage('/Images/pin-main4.png')
-        }
-    }
+const PinnEffects = ({ stack }: MyComponentProps) => {
+
     const isMobile = useIsMobile()
     return (
         <Box p={isMobile ? '2rem 0rem 2rem 2rem;' : '4rem 4rem 0rem 0rem'} className="right" >
             <Flex flexDirection="column" justifyContent='start' alignItems='start'>
                 {
-                    hover === 1 ? (
+                    stack === 1 ? (
                         <div >
                             <PinnedSection title='Engage' image={'/Images/pin-mini1.png'} description='Engage with Nexus community and compete with others in sharing the upcoming launches for a chance to win a spot.' />
                         </div>
                     ) : (
-                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div onClick={() => onMouse(1)} style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Engage</div></Flex>
+                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Engage</div></Flex>
                     )
                 }
                 {
-                    hover === 2 ? (
+                    stack === 2 ? (
                         <div >
                             <PinnedSection title='Snapshot' image={'/Images/pin-mini2.png'} description='Snapshots are taken of all the competing profiles and wallets.' />
                         </div>
                     ) : (
-                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div onClick={() => onMouse(2)} style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Snapshot</div></Flex>
+                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Snapshot</div></Flex>
                     )
                 }
                 {
-                    hover === 3 ? (
+                    stack === 3 ? (
                         <div >
                             <PinnedSection title='Lottery' image={'/Images/pin-mini3.png'} description='Nexus smart contract withdraws a lottery for each launch and selects limited random profiles which will be investing.' />
                         </div>
                     ) : (
-                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div onClick={() => onMouse(3)} style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Lottery</div></Flex>
+                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Lottery</div></Flex>
                     )
                 }
                 {
-                    hover === 4 ? (
+                    stack === 4 ? (
                         <div >
                             <PinnedSection title='Contribution' image={'/Images/pin-mini4.png'} description='Selected profiles and wallets invest their funds into the sale and claim purchased tokens according to the vesting period.' />
                         </div>
                     ) : (
-                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div onClick={() => onMouse(4)} style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Contribution</div></Flex>
+                        <Flex className="hover-scale" style={{ marginLeft: '2.3rem', cursor: 'pointer', position: 'relative', zIndex: 1000, gap: '66px' }} gap="3rem"><div className='dot'></div><div style={{ textAlign: 'start', fontSize: '25px', fontFamily: 'SEN medium', color: DEFAULT_COLORS.White }}>Contribution</div></Flex>
                     )
                 }
             </Flex>
--- src/app/view/landing/PinSectionCompoent.tsx
+++ src/app/view/landing/PinSectionCompoent.tsx
@@ -2,42 +2,116 @@ import { Box, Grid, Container } from '@mui/material';
 import Flex from '../../components/utility-components/flex/Flex';
 import Image from '../../components/utility-components/image/Image';
 import { IMAGE_COLLECTIONS } from '@/app/utils/images';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RightShadow from '@/app/components/page-components/RightShadow';
 import PinnEffects from '@/app/components/page-components/PinnEffects';
 import useIntersectionAnimation from '@/app/hooks/useIntersectionAnimation';
 import useIsMobile from '@/app/hooks/useIsMobile';
 
+
 function PinSectionCompoent() {
-
-  const [pinnedImage, setPinnedImage] = React.useState(IMAGE_COLLECTIONS.PinSectionImage)
-
-  const boxRef = useIntersectionAnimation();
   const isMobile = useIsMobile()
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const viewportHeight = window.innerHeight;
+      const numberOfImages = 4; // Assuming you have 4 images
+      const imageHeight = viewportHeight; // Each image takes the full height of the viewport
+
+      // Log the scroll position and image height
+      console.log("Scroll Position:", scrollPosition);
+      console.log("Viewport Height:", viewportHeight);
+
+      // Calculate which image should be displayed based on the scroll position
+      const imageIndexToShow = Math.floor(scrollPosition / imageHeight);
+
+      // Log the calculated index
+      console.log("Image Index:", imageIndexToShow);
+
+      // Ensure the image index doesn't go beyond the number of images
+      setCurrentImage(imageIndexToShow);
+      console.log('current image', currentImage);
+
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [currentImage]);
+
+
   return (
 
     <>
       <Box position={'absolute'} right="0px" top="1200px" >
         <RightShadow />
       </Box>
-      <Flex style={{ height: isMobile ? '100%' : '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Box className="contain" ref={boxRef}>
-          <Grid container>
-            <Grid item lg={6} sm={6} xs={12}>
-              <Flex>
-                <Box p={isMobile ? '2rem' : '4rem 4rem 0rem 0rem'} className="left">
-                  <Image src={pinnedImage} width="95%" /> 
-                </Box>
-              </Flex>
-            </Grid>
-            <Grid item lg={6} sm={6} xs={12}>
-              <PinnEffects setPinnedImage={setPinnedImage} />
-            </Grid>
-          </Grid>
+      <Box sx={{ height: isMobile ? '800vh' : '800vh', position: 'relative', width: '100%' }}>
+        <Box
+          position={'sticky'}
+          top={'50%'}
+          left={'0%'}
+          zIndex={1}
+
+          sx={{
+            marginTop: '50vh',
+            marginBottom: '50vh',
+
+          }}>
+          <Box position={'relative'} display={'flex'} justifyContent={'center'} alignItems={'center'} ml={isMobile ? '' : '5%'}>
+            <Content
+              stack={
+                currentImage <= 3 ? 1 :
+                  (currentImage === 4 || currentImage === 5) ? 2 :
+                    (currentImage === 6 || currentImage === 7) ? 3 :
+                      (currentImage >= 8) ? 4 :
+                        0
+              }
+            />
+
+          </Box>
         </Box>
-      </Flex>
+      </Box>
     </>
   );
 }
 
 export default PinSectionCompoent;
+
+
+const Content = ({ stack }: any) => {
+
+  const [pinnedImage, setPinnedImage] = React.useState(IMAGE_COLLECTIONS.PinSectionImage)
+
+  const boxRef = useIntersectionAnimation();
+  const isMobile = useIsMobile()
+  useEffect(() => {
+    if (stack === 1) {
+      setPinnedImage('/Images/pin-main1.png')
+    } else if (stack === 2) {
+      setPinnedImage('/Images/pin-main2.png')
+    }
+    else if (stack === 3) {
+      setPinnedImage('/Images/pin-main3.png')
+    }
+    else {
+      setPinnedImage('/Images/pin-main4.png')
+    }
+  }, [stack])
+
+  return (
+    <Grid container sx={{ position: 'absolute' }}>
+      <Grid item lg={6} sm={6} xs={12}>
+        <Flex justifyContent='end'>
+          <Box p={isMobile ? '1rem' : '0rem'} mt={'3rem'} className="left">
+            <Image src={pinnedImage} width={isMobile ? '100%' : '85%'} />
+          </Box>
+        </Flex>
+      </Grid>
+      <Grid item lg={6} sm={6} xs={12}>
+        <PinnEffects stack={stack} />
+      </Grid>
+    </Grid>
+  )
+}
